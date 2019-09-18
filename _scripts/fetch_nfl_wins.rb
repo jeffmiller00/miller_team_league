c63800e1254768201aa7eafce74c421058e80915
Jeff Miller <jmiller@ncsasports.org>
2019-09-18 12:08:00 -0500

    First attempt at moving the fetch wins script over here.

---
+++ _scripts/fetch_nfl_wins.rb
@@ -0,0 +1,106 @@
+require 'httpi'
+require 'nokogiri'
+require 'json'
+require 'yaml'
+require 'pry'
+
+DATA_FILE = '../_data/nfl_2019.json'
+
+def get_teams
+  all_teams = File.read(DATA_FILE)
+  JSON.parse(all_teams)
+end
+
+def generate_team_table
+  all_teams = get_teams
+  player_picks = YAML.load(File.read('../_data/index/nfl_picks.yml'))
+
+  player_picks.each do |player|
+    player['teams'].each do |team|
+      truth_team = all_teams.find{ |team_hash| team_hash['name'] == team['name'].split.last }
+      team['wins'] = truth_team['wins'].map{ |wins_on| wins_on[1] }.max
+    end
+  end
+  puts player_picks.to_yaml
+end
+
+EMPTY_WEEK = {jeff: 0, greg: 0, tim: 0, zach: 0, mike: 0}
+CURRENT_NFL_WEEK = 2
+
+def generate_summary_chart
+  all_teams = get_teams
+  weeklySummary = []
+  week1begin = Date.parse('2019-09-03')
+  week1end   = Date.parse('2019-09-10')
+  CURRENT_NFL_WEEK.times do |i|
+    weeklySummary[i] = EMPTY_WEEK.dup
+    weekBegin = week1begin.next_day(7*i)
+    weekEnd   = week1end.next_day(7*i)
+    weeklySummary[i][:weekNum] = i+1
+    weeklySummary[i][:dates] = (weekBegin..weekEnd)
+  end
+
+  all_teams.each do |team|
+    next if team['drafted_by'].empty?
+    brother = team['drafted_by'].downcase
+    team['wins'].each do |date, wins|
+      weeklySummary.each do |summary|
+        if summary[:dates].include?(Date.parse(date))
+          summary[brother.to_sym] += wins
+        end
+      end
+    end
+  end
+
+  allSummaries = []
+  allSummaries << weeklySummary.map{ |summary| summary[:jeff] }
+  allSummaries << weeklySummary.map{ |summary| summary[:greg] }
+  allSummaries << weeklySummary.map{ |summary| summary[:tim] }
+  allSummaries << weeklySummary.map{ |summary| summary[:zach] }
+  allSummaries << weeklySummary.map{ |summary| summary[:mike] }
+  puts allSummaries.to_s
+end
+
+def fetch_wins?; true; end;
+def write_file?; false; end;
+
+if fetch_wins?
+  request = HTTPI::Request.new
+  request.url = 'https://www.cbssports.com/nfl/standings/'
+  # request.query = { Season: '2015-16',
+  #                   SeasonType: 'Regular%20Season'}
+  response = HTTPI.get(request)
+  full_doc = Nokogiri::HTML(response.body)
+  truth_teams = full_doc.xpath('//table[@class="TableBase-table"]/tr')
+
+
+  all_teams = File.read(DATA_FILE)
+  all_teams = JSON.parse(all_teams)
+
+  all_teams.each do |team|
+    if truth_teams.search("[text()*='#{team['location']}']").first
+      wins = truth_teams.search("[text()*='#{team['location']}']").first.parent.parent.parent.parent.parent.parent.children[2].text.to_i
+      puts "#{team['location']} | #{wins}"
+    elsif truth_teams.search("[text()*='#{team['name']}']").first
+      wins = truth_teams.search("[text()*='#{team['name']}']").first.parent.parent.parent.parent.parent.parent.children[2].text.to_i
+      puts "#{team['location']} | #{wins}"
+    else
+      binding.pry
+    end
+
+    team['wins'][Date.today.prev_day.to_s] = wins
+  end
+
+  if write_file?
+    FileUtils.copy(DATA_FILE, "./archive/nfl_#{Date.today}.json")
+    File.open(DATA_FILE,"w") do |f|
+      f.write(all_teams.to_json)
+    end
+  else
+    puts all_teams.to_json
+  end
+end
+
+generate_team_table
+puts '---------------------------||-------------------------'
+generate_summary_chart
