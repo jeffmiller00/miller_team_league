1aa8ef0a2c9c909cbb5f3232f6c0ea542d72da23
Jeff Miller <jmiller@ncsasports.org>
2019-11-19 12:57:06 -0600

    Don't send ruby files to s3.

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -2,4 +2,4 @@ git push origin master
 jekyll clean
 jekyll build
 aws s3 rm s3://millerteamleague.com --recursive
-aws s3 cp ./_site/. s3://www.millerteamleague.com --recursive --exclude "*.sh"
+aws s3 cp ./_site/. s3://www.millerteamleague.com --recursive --exclude "*.sh" --exclude "*.rb"
