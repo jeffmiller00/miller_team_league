34c6b188621a0c6df9f31b69c7294e01e27a8a9f
Jeff Miller <jeffmiller00@gmail.com>
2021-08-16 22:56:30 -0500

    Added a package lock & remove the old deployment script.

--- upload_to_s3.sh
+++
@@ -1,5 +0,0 @@
-git push origin master
-jekyll clean
-jekyll build
-aws s3 rm s3://millerteamleague.com --recursive
-aws s3 cp ./_site/. s3://www.millerteamleague.com --recursive --exclude "*.sh" --exclude "*.rb"
